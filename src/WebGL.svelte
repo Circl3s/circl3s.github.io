7bc07839397b9466806ca3dcfb4bf288a73b3525
Circl3s <maak4422@gmail.com>
2021-07-01 00:06:46 +0200

    test fix

--- src/WebGL.svelte
+++ src/WebGL.svelte
@@ -28,9 +28,9 @@ void main() {
 
         var gl = canvas.getContext("webgl");
 
-        if (!gl) {
-            gl = canvas.getContext("experimental-webgl");
-        }
+        // if (!gl) {
+        //     gl = canvas.getContext("experimental-webgl");
+        // }
 
         gl.clearColor(0.75, 0.85, 0.8, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
